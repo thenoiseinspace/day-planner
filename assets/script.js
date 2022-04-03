9a08f0a75c726d0fddca8cdd024daa568ce0763a
Sarah Gilbreath <thenoiseinspace@gmail.com>
2022-04-03 00:46:43 -0400

    not sure the last push worked so trying one last time to get the commenting live

--- assets/script.js
+++ assets/script.js
@@ -2,26 +2,14 @@
 //DAY AND TIME FUNCTIONALITY//
 //////////////////////////////
 
-// This displays the date and time at the top of the page
+//Setting up variables first
 var today = moment();
-$('#currentDay').text(today.format('dddd, MMM Do, YYYY'));
 var currentTime = moment();
+
+// This displays the date and time at the top of the page
+$('#currentDay').text(today.format('dddd, MMM Do, YYYY'));
 $("#currentTime").text(currentTime.format('hh:mm a'));
 
-////////////////////////////
-///////LOCAL STORAGE///////
-////////////////////////////
-
-// This uses the ID and class from each of the time blocks to identify the right text block and get it from local storage
-$('#9 .thingToDo').val(localStorage.getItem('9'));
-$('#10 .thingToDo').val(localStorage.getItem('10'));
-$('#11 .thingToDo').val(localStorage.getItem('11'));
-$('#12 .thingToDo').val(localStorage.getItem('12'));
-$('#13 .thingToDo').val(localStorage.getItem('13'));
-$('#14 .thingToDo').val(localStorage.getItem('14'));
-$('#15 .thingToDo').val(localStorage.getItem('15'));
-$('#16 .thingToDo').val(localStorage.getItem('16'));
-$('#17 .thingToDo').val(localStorage.getItem('17'));
 
 ////////////////////////////
 //SAVE BUTTON FUNCTIONALITY//
@@ -33,13 +21,28 @@ var saveButtonEl = $('.save-button');
 //When the buttons are clicked, it runs the function that calls the parent element and selects the attribute 
 //The button's sibling is the neighboring text field in the same div. Targets the value.
 //Sets to local storage 
+//Credit to James and especially to Lina for helping explain this part to me. I had originally created an empty <p> tag in each row and was then trying to append the content. Using jQuery here is much faster.  
 saveButtonEl.on('click', function() {
     var time = $(this).parent().attr('id');
     var thingToDo = $(this).siblings('.thingToDo').val();
-    console.log(time);
-    console.log(thingToDo);
     localStorage.setItem(time, thingToDo);
 })
+
+////////////////////////////
+///////LOCAL STORAGE///////
+////////////////////////////
+
+// This uses the ID and class from each of the time blocks to identify the right text block and get it from local storage
+//Please note - a BCS expert gave our study group this chunk of code
+$('#9 .thingToDo').val(localStorage.getItem('9'));
+$('#10 .thingToDo').val(localStorage.getItem('10'));
+$('#11 .thingToDo').val(localStorage.getItem('11'));
+$('#12 .thingToDo').val(localStorage.getItem('12'));
+$('#13 .thingToDo').val(localStorage.getItem('13'));
+$('#14 .thingToDo').val(localStorage.getItem('14'));
+$('#15 .thingToDo').val(localStorage.getItem('15'));
+$('#16 .thingToDo').val(localStorage.getItem('16'));
+$('#17 .thingToDo').val(localStorage.getItem('17'));
 
 ////////////////////////////
 //////CHANGING COLORS//////
